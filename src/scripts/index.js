adfb3f481c36ed04eb9a052c03442b1169ac2eb2
Noor Khan <khannoor710@gmail.com>
2021-03-31 16:32:40 +0530

    Javascript basic practice

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,37 @@
-import '../styles/index.scss';
-
-if (process.env.NODE_ENV === 'development') {
-  require('../index.html');
+function startCars(car1, car2, car3, ...rest){
+  console.log(car1,car2, car3, rest);
 }
 
-console.log('webpack starterkit');
+let carIds = [1,2,3,4,5];
+startCars(...carIds);
+
+function stopCars(...rest){
+  console.log(typeof(rest));
+}
+
+let restArray = [7,8];
+stopCars(...restArray);
+
+console.log(Number.parseInt('55ABC'));
+console.log(Number.parseFloat('55.88ABC'));
+console.log(Number.parseFloat('ABS55.88ABC'));
+
+
+let i =0;
+for(; i < 10;i++){
+  console.log(i);
+  if(i===4)
+  break;
+}
+console.log("");
+for (let i = 0; i < 5; i++) {
+  
+  if(i === 3)
+    continue;
+  console.log(i);
+}
+
+let userSettings = null;
+let DefaultSettings = {name: 'Default'};
+
+console.log(userSettings || DefaultSettings);
